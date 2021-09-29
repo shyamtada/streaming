7a012882027932118c6dc0a5a2e50c73ac238596
shyamtada <shyam@digitaltrons.com>
2021-09-29 11:09:04 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPlayer
+        width="50%"
+        height="50%"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        url="https://ac8adf168f69.us-west-2.playback.live-video.net/api/video/v1/us-west-2.473199066310.channel.H4sy5NggSpIp.m3u8"
+        playing={true}
+        controls={true}
+      />
     </div>
   );
 }
